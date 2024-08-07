7c3d415924fd6b41dfb15be4037e89fc107e92ec
nahom <nbirhane@gmail.com>
2024-08-07 17:49:14 -0500

    eRegistrarWebAPI Project Added

---
+++ src/main/resources/registration.sql
@@ -0,0 +1,11 @@
+-- Insert data into the students table
+INSERT INTO students (student_number, first_name, middle_name, last_name, cgpa, enrollment_date, is_international)
+VALUES
+    ('S001', 'Nahom', 'A.', 'Birhane', 3.8, '2020-09-01', 'YES'),
+    ('S002', 'Aron', 'B.', 'Bereket', 3.5, '2019-09-01', 'NO'),
+    ('S003', 'Semhar', 'C.', 'Haile', 3.9, '2021-01-15', 'YES'),
+    ('S004', 'Alice', 'D.', 'Johnson', 3.7, '2020-09-01', 'NO'),
+    ('S005', 'Michal', 'E.', 'Tesfu', 3.6, '2019-09-01', 'NO'),
+    ('S006', 'Mengis', 'F.', 'Clark', 3.4, '2021-01-15', 'YES'),
+    ('S007', 'Jossie', 'G.', 'Davis', 3.5, '2020-09-01', 'NO'),
+    ('S008', 'Mussie', 'H.', 'Tekle', 3.8, '2019-09-01', 'YES');
